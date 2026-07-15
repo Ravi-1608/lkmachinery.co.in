614a3914426bf979d733ccb8df6cc21eee062100
Ravi-1608 <maneravi3332@gmail.com>
2026-07-15 11:26:39 +0530

    M6 fix: add logo asset and reinstate Organization schema logo field

--- lib/seo.ts
+++ lib/seo.ts
@@ -4,8 +4,7 @@ export function getOrganizationSchema() {
     "@type": "Organization",
     name: "L.K. Machinery India Pvt. Ltd.",
     url: "https://www.lkmachinery.co.in",
-    // FIXME: Add a real logo file to public/images/logo.png and reinstate this field
-    // logo: "https://www.lkmachinery.co.in/images/logo.png",
+    logo: "https://www.lkmachinery.co.in/images/logo.png",
     contactPoint: {
       "@type": "ContactPoint",
       telephone: "+91-8888718587",
